ec1969d8d6dd7acf62091d81a053dc5e1442218d
Jofer Usa <joferusa43@gmail.com>
2024-07-28 06:42:01 +0800

    add loader

---
+++ hooks/date.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+const useDate = () => {
+  const [formattedDate, setFormattedDate] = useState(null);
+
+  useEffect(
+    () => setFormattedDate(new Date().toLocaleDateString("en-US")),
+    []
+  );
+
+  return formattedDate;
+};
+
+export default useDate;
