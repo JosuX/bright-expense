192fb3464b09b3572f25d4403062b028da09dbb0
Jofer Usa <joferusa43@gmail.com>
2024-07-28 05:47:05 +0800

    add date-fns

--- db/expense.ts
+++ db/expense.ts
@@ -27,6 +27,7 @@ const getMonthlyExpense = async (date: Date) => {
 };
 
 const getDailyExpense = async (date: Date, page: number, pageSize: number) => {
+	date.setDate(date.getDate() + 1);
 	const startDate = startOfDay(date);
 	const endDate = endOfDay(date);
 
